517c51226da00fade304941d5ebcbdd91d7d5c5e
Mihika <mihika.shilpi@me.com>
2018-11-14 22:33:07 +0100

    "trial"

---
+++ app/Http/Controllers/SigController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SigController extends Controller
+{
+    public function index()
+    {
+        return 'Here are all the books...';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return "yo";
-});
+Route::get('/', 'SigController@index');
