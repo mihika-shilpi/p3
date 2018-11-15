0efb3bc1630957f753d37743f50d9be5867f5b31
Mihika <mihika.shilpi@me.com>
2018-11-15 05:08:47 +0100

    image url fix

--- resources/views/generate.blade.php
+++ resources/views/generate.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="bandeau" style="background-image:url(Images/IMG_3289.jpg)">
+    <div class="bandeau" style="background-image:url(/images/IMG_3289.jpg)">
         <h1 class="white">Ajit Shilpi's Signature Architect</h1>
         <p class="white">Generating your company email signature</p>
     </div>
